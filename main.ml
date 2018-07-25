ed24c37b1f698fafbd62b82708c5d5c877483d4b
Marcin Wolicki <marcin.wolicki@gmail.com>
2018-07-24 23:31:22 -0700

    splitted implementation to seperate files/modules

--- main.ml
+++ main.ml
@@ -1,10 +1,5 @@
-type objectref = ObjectRef of int32
-type arrayref = ArrayRef of int32
-
-type count = Count of int32
-type length = Length of int32
-type 'a value = Value
-type index_ref = int32 value
+open Types
+open Class_file
 
 type _ op =
   | Aaload : arrayref op * index_ref op -> _ value op (** load onto the stack a reference from an array *)
@@ -58,93 +53,6 @@ type file_version = {
   minor_version : int;
   major_version : int;
 }
-
-let cont f (error:string) cont' = function
-  | Ok x -> begin
-    match f x with
-    | Some (value, remaining) -> cont' value remaining
-    | None -> Error error end
-  | Error e -> Error e
-
-let get_2u = cont (function
-  | x::y::z -> Some (x lsl 8 + y, z)
-  | _ -> None)
-
-let get_4u = cont (function
-  | a::b::c::d::z -> Some ((a lsl 24) + (b lsl 16) + (c lsl 8) + d, z)
-  | _ -> None)
-
-
-type class_name = ClassName of string
-type _ baseType =
-| BtByte : bytes baseType
-| BtChar : char baseType
-| BtDouble : float baseType
-| BtFloat : float baseType (* not sure what type to use for single-precision floating point *)
-| BtInteger : int32 baseType
-| BtLong : int64 baseType
-| BtShort : Stdint.int16 baseType
-| BtBoolean : bool baseType
-| BtReference : class_name -> objectref baseType
-| BtArrayReference : arrayref baseType
-
-type len = Stdint.uint16 (*u2*)
-type str = Stdint.uint8 list (*u1 list*)
-
-type constant =
-| CClass 
-| CFiledRef
-| CMethodRef
-| CInterfaceMethodRef
-| CString
-| CInteger
-| CFloat
-| CLong
-| CDouble
-| CNameAndType
-| CUtf8 of int * int list
-| CMethodHandle
-| CMethodType
-| CInvokeDynamic
-
-module List = struct
-  let part i =
-    let rec part' i acc l = 
-      if i = 0 then acc |> List.rev, l
-      else
-        match l with
-        | [] -> acc |> List.rev, []
-        | x :: xs -> part' (i - 1) (x::acc) xs
-    in part' i []
-end
-
-let (||>) (a:'a*'b) f = f a
-
-let try_get_constant = function
-  | 1 :: x :: y :: r -> 
-    let len = x lsl 8 + y in
-    let (str, r) = r |> List.part len in
-    (CUtf8 (len, str), r) |> Ok
-  | 3 :: x -> failwith "todo"
-  | x :: _ -> Printf.sprintf "usnuported constant tag = %i" x |> Error
-  | [] ->Error "usnuported constant tag = <empty stream of data>"
-
-let base_type_of_char: type a. char -> a baseType option = function
-| 'B' -> Some BtByte
-| 'C' -> Some BtChar
-| _ -> None
-
-
-
-let parse_class_file x =
-  Ok x
-  |> get_4u "missing magic number" (fun magic_number rem -> if magic_number = 0xCAFEBABE then Ok rem else Error "wrong magic number")
-  |> get_2u "missing minor_version" (fun minor_version rem ->
-    Ok rem |> get_2u "missing major_verion" (fun major_version rem ->
-      let _ = {minor_version; major_version} in
-      Ok rem))
-  |> get_2u "missing const_pool_count" (fun constant_pool_count _rem -> Error (string_of_int constant_pool_count))
-
 
 let () =
   read_file "/Users/kevin/_projects/java/HelloWorld.class" 
---
+++ types.ml
@@ -0,0 +1,7 @@
+type objectref = ObjectRef of int32
+type arrayref = ArrayRef of int32
+
+type count = Count of int32
+type length = Length of int32
+type 'a value = Value
+type index_ref = int32 value
