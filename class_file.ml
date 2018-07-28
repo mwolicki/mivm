5d18421fb0a006e066723b8cb0ddc7f97f557844
Marcin Wolicki <marcin.wolicki@gmail.com>
2018-07-28 00:25:06 -0700

    basic support of consts

--- class_file.ml
+++ class_file.ml
@@ -85,6 +85,38 @@ type constant =
 | CMethodType of descriptor_index
 | CInvokeDynamic of bootstrap_method_attr_index * name_and_type_index
 
+
+let as_int = Stdint.Uint16.to_int
+
+let to_str_constant = function
+| CClass name_index -> 
+    Printf.sprintf "CClass (name_index = %i)" (as_int name_index)
+| CFiledRef (class_index, name_and_type_index) -> 
+    Printf.sprintf "CFiledRef (class_index = %i, name_and_type_index = %i)" (as_int class_index) (as_int name_and_type_index)
+| CMethodRef (class_index, name_and_type_index) -> 
+    Printf.sprintf "CMethodRef (class_index = %d, name_and_type_index %d)" (as_int class_index) (as_int name_and_type_index)
+| CInterfaceMethodRef (class_index, name_and_type_index) -> 
+    Printf.sprintf "CInterfaceMethodRef (class_index = %d, name_and_type_index %d)" (as_int class_index) (as_int name_and_type_index)
+| CString string_index -> 
+    Printf.sprintf "CString (string_index = %d)" (as_int string_index)
+| CInteger int32 -> 
+    Printf.sprintf "CInteger %s" (Int32.to_string int32)
+| CFloat float -> 
+    Printf.sprintf "CFloat %f" float
+| CLong int64 -> 
+    Printf.sprintf "CLong %s" (Int64.to_string int64)
+| CDouble float  -> 
+    Printf.sprintf "CDouble %f" float
+| CNameAndType (name_index, descriptor_index) -> 
+    Printf.sprintf "CNameAndType (name_index = %d, descriptor_index = %d)" (as_int name_index) (as_int descriptor_index)
+| CUtf8 s -> 
+    Printf.sprintf "CUtf8 %s" s
+| CMethodHandle (_reference_kind, reference_index) -> 
+    Printf.sprintf "CMethodHandle (KIND, reference_index = %d)"  (as_int reference_index)
+| CMethodType descriptor_index -> 
+    Printf.sprintf "CMethodType descriptor_index = %d" (as_int descriptor_index)
+| CInvokeDynamic (bootstrap_method_attr_index, name_and_type_index) -> 
+    Printf.sprintf "CInvokeDynamic (bootstrap_method_attr_index = %d, name_and_type_index = %d)" (as_int bootstrap_method_attr_index) (as_int name_and_type_index)
 module List = struct
   include List
   let part i =
@@ -125,9 +157,42 @@ let try_get_constant = function
   | 5:: a :: b :: c :: d :: e :: f :: g :: h :: r -> Ok (get_int64 a b c d e f g h |> CLong, r)
   | 6:: _a :: _b :: _c :: _d :: _e :: _f :: _g :: _h :: r -> Ok (CDouble 0.0, r)  (* TODO *)
   | 7:: a :: b :: r -> Ok (get_uint16 a b |> CClass, r)
+  | 8:: a :: b :: r -> Ok (get_uint16 a b |> CString, r)
+  | 9:: a :: b :: d :: e :: r -> Ok (CFiledRef(get_uint16 a b, get_uint16 d e), r)
+  | 10:: a :: b :: d :: e :: r -> Ok (CMethodRef(get_uint16 a b, get_uint16 d e), r)
+  | 11:: a :: b :: d :: e :: r -> Ok (CInterfaceMethodRef(get_uint16 a b, get_uint16 d e), r)
+  | 12:: a :: b :: d :: e :: r -> Ok (CNameAndType(get_uint16 a b, get_uint16 d e), r)
+  | 15:: kind' :: a :: b :: r -> 
+    let kind = match kind' with
+               | 1 -> Some REF_getField
+               | 2 -> Some REF_getStatic
+               | 3 -> Some REF_putField
+               | 4 -> Some REF_putStatic
+               | 5 -> Some REF_invokeVirtual
+               | 6 -> Some REF_invokeStatic
+               | 7 -> Some REF_invokeSpecial
+               | 8 -> Some REF_newInvokeSpecial
+               | 9 -> Some REF_invokeInterface
+               | _ -> None               
+    in begin match kind with
+    | Some kind -> Ok (CMethodHandle(kind, get_uint16 a b), r)
+    | None -> Printf.sprintf "unknown kind = %i" kind' |> Error end
+  | 16:: a :: b :: r -> Ok (CMethodType(get_uint16 a b), r)
+  | 18:: a :: b :: d :: e :: r -> Ok (CInvokeDynamic(get_uint16 a b, get_uint16 d e), r)
   | x :: _ -> Printf.sprintf "usnuported constant tag = %i" x |> Error
   | [] ->Error "usnuported constant tag = <empty stream of data>"
 
+
+let rec try_get_constants no (data:int list) =
+    if no = 0 then Ok([], data)
+    else
+        match try_get_constant data with
+        | Ok (result, remaining) ->
+            begin match try_get_constants (no - 1) remaining with
+            | Ok (tail, remaining) -> Ok (result::tail, remaining)
+            | Error error -> Error error end
+        | Error e -> Error e
+        
 (* let base_type_of_char: type a. char -> a baseType option = function
 | 'B' -> Some BtByte
 | 'C' -> Some BtChar
@@ -142,5 +207,7 @@ let parse_class_file x =
     Ok rem |> get_2u "missing major_verion" (fun major_version rem ->
       let _ = {minor_version; major_version} in
       Ok rem))
-  |> get_2u "missing const_pool_count" (fun constant_pool_count _rem -> Error (string_of_int constant_pool_count))
-
+  |> get_2u "missing const_pool_count" (fun constant_pool_count rem -> 
+        match try_get_constants (constant_pool_count - 1) rem with
+        | Ok (x, _) -> x |> List.map to_str_constant |> String.concat ", " |> Error
+        | Error x -> Error x)
